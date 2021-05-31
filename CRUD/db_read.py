ecaa5d36513e67b08a14a131d54a460bea75f6c8
Stiffer Doroskevich <stiffer17@gmail.com>
2021-05-31 17:26:03 -0400

    Cleaned comments from db_read

--- CRUD/db_read.py
+++ CRUD/db_read.py
@@ -7,19 +7,6 @@ from bson.json_util import dumps
 client = Connect.get_connection()
 
 
-'''print('Printing first 10 documents...')
-for doc in documents[:10]:
-    country = doc['address']['country']
-    name = doc['name']
-    print(f'Country: {country}, Name: {name}')'''
-
-#10006546 - 10009999
-'''def print_by_id(id):
-    document = col.find_one({'_id':str(id)})
-    doc_basic_info = document.get('name'), document.get('room_type'), document.get('price')
-    print(doc_basic_info)
-'''
-
 def all_stores():
     collection = client['price_analytics']['stores']
     col_results = json.loads(dumps(collection.find().limit(0).sort("time", -1)))
